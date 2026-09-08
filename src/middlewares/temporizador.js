2d8dfeb8a3e4b6db94ded058e9cddf064b0172f3
Ana Livia <livia.silva36@aluno.senai.br>
2026-09-08 11:15:38 -0300

    semana 11 dia 2

---
+++ src/middlewares/temporizador.js
@@ -0,0 +1,13 @@
+function temporizador(req,res,next){
+    const inicio = date.now();
+    const mmetodo = req.method;
+    const url = req.originalUrl; || req.url;
+
+    res.on("finish", () => {
+        const duracao = date.now() - inicio;
+        console.log('[TEMPORIZADOR] ${metodo} ${url} - ${duracao} ms');
+
+    });
+next();
+}
+module.exports = temporizador;
